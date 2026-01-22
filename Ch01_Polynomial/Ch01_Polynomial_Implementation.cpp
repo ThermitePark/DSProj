15c515d21d21524589372ed77c939ebcf009ebc8
ThermitePark <hjp78940@gmail.com>
2026-01-22 21:22:26 +0900

    Ch01 Polynomial Implementation file

---
+++ Ch01_Polynomial/Ch01_Polynomial_Implementation.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include "Ch01_Polynomial.h"
+using namespace std;
+
+istream& operator>> (istream& is, Polynomial& p) {
+
+	int noofterms;
+	float coef;
+	int exp;
+	is >> noofterms;
+	for (int i = 0; i < noofterms; i++) {
+		is >> coef >> exp;
+		p.NewTerm(coef, exp);
+	}
+	return is;
+}
+
+ostream& operator<< (ostream& os, Polynomial& p) {
+	for (int i = 0; i < p.terms; i++) {
+		if (p.termArray[i].exp >= 2) {
+			if (p.termArray[i].coef == 1) {
+				os << "x^" << p.termArray[i].exp;
+			}
+			else if (p.termArray[i].coef == -1) {
+				os << "-x^" << p.termArray[i].exp;
+			}
+			else {
+				os << p.termArray[i].coef << "x^" << p.termArray[i].exp;
+			}
+
+
+		}
+		else if (p.termArray[i].exp == 1) {
+			if (p.termArray[i].coef == 1) {
+				os << "x";
+			}
+			else if (p.termArray[i].coef == -1) {
+				os << "-x";
+			}
+			else {
+				os << p.termArray[i].coef << "x";
+			}
+		}
+		else {
+			os << p.termArray[i].coef;
+			if (i < p.terms - 1) {
+				if (p.termArray[i + 1].coef > 0) {
+					os << " +";
+				}
+				else {
+					os << "  ";
+				}
+			}
+		}
+	}
+	os << endl;
+
+	return os;
+}
+
+Polynomial::Polynomial() : capacity(1), terms(0) {
+	termArray = new Term[capacity];
+}
+
+void Polynomial::NewTerm(const float theCoeff, const int theExp) {
+	if (terms == capacity) {
+		capacity *= 2;
+		Term* temp = new Term[capacity];
+		copy(termArray, termArray + terms, temp);
+		termArray = temp;
+	}
+
+	termArray[terms].coef = theCoeff;
+	termArray[terms++].exp = theExp;
+}
+
+Polynomial Polynomial::operator+(Polynomial& b) {
+	Polynomial c;
+	int aPos = 0, bPos = 0;
+	while ((aPos < terms) && (bPos < b.terms)) {
+		if (termArray[aPos].exp == b.termArray[aPos].exp) {
+			float t = termArray[aPos].coef + b.termArray[bPos].coef;
+			if (t) c.NewTerm(t, termArray[aPos].exp);
+			aPos++; bPos++;
+		}
+		else if (termArray[aPos].exp < b.termArray[bPos].exp) {
+			c.NewTerm(b.termArray[bPos].coef, b.termArray[bPos].exp);
+			bPos++;
+		}
+		else {
+			c.NewTerm(termArray[aPos].coef, termArray[aPos].exp);
+			aPos++;
+		}
+	for (; aPos < terms; aPos++) {
+		c.NewTerm(termArray[aPos].coef, termArray[aPos].exp);
+	}
+	for (; bPos < b.terms; bPos++) {
+		c.NewTerm(termArray[bPos].coef, termArray[bPos].exp);
+	}
+
+	return c;
+
+
+	}
+}
