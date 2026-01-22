215118faab71457af9754db3b4eb87d729a0aec0
ThermitePark <hjp78940@gmail.com>
2026-01-22 21:21:57 +0900

    Ch01 header file

---
+++ Ch01_Polynomial/Ch01_Polynomial.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef CH01_POLYNOMIAL_H
+#define CH01_POLYNOMIAL_H
+using namespace std;
+
+class Polynomial;
+
+class Term {
+	friend class Polynomial;
+	friend ostream& operator<<(ostream&, Polynomial&);
+	friend istream& operator>>(istream&, Polynomial&);
+private:
+	float coef;
+	int exp;
+};
+
+class Polynomial {
+public:
+	Polynomial();
+	Polynomial operator+(Polynomial&);
+	void NewTerm(const float, const int);
+	friend ostream& operator<<(ostream&, Polynomial&);
+	friend istream& operator>>(istream&, Polynomial&);
+private:
+	Term* termArray;
+	int capacity;
+	int terms;
+};
+
+#endif
+
