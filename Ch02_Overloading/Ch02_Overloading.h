081a1f3bb7335ef52507d2dc499b15495ce23729
ThermitePark <hjp78940@gmail.com>
2026-01-24 19:22:24 +0900

    Ch02 header file

---
+++ Ch02_Overloading/Ch02_Overloading.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef CH02_OVERLOADING_H
+#define CH02_OVERLOADING_H
+class Rectangle {
+public:
+	Rectangle(int, int, int, int);
+	~Rectangle();
+	void Print();
+	int Area();
+	bool LessThan(Rectangle&);
+	bool EqualSize(Rectangle&);
+	bool Equal(Rectangle&);
+	int GetHeight();
+	int GetWidth();
+private:
+	int xLow, yLow, height, width;
+};
+#endif
